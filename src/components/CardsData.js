dfb70fe41dd9473fcad72d78e2700c7a4a0eeafb
youssef lehmi <92147661+pgm-yousseflehmi@users.noreply.github.com>
2022-06-16 17:54:07 +0200

    Add files via upload

---
+++ src/components/CardsData.js
@@ -0,0 +1,124 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "burger",
+        imgdata: "https://www.gardengourmet.be/sites/default/files/recipes/aeead5804e79ff6fb98b2039619c5230_200828_MEDIAMONKS_GG_Spicytarian.jpg",
+        address: "american food",
+        somedata: " 1175 + order placed from here recently",
+        price: 15,
+        rating: "3.8",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: " chicken burger",
+        imgdata: "https://www.burgerking.nl/007_nl/products_nl/image-thumb__29704__product_detail/Product-CrispyChicken-2104-1500px.png",
+        address: "Street Food",
+        somedata: " 2525 + order placed from here recently",
+        price: 10,
+        rating: "3.9",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "familie burger",
+        imgdata: "https://kfcrestaurants.be/wp-content/uploads/2020/09/Family-burger-boxmeal-voor-2-1corn-1200x800px.jpg",
+        address: "Food",
+        somedata: " 2525 + order placed from here recently",
+        price: 45,
+        rating: "3.9",
+        qnty:0
+    },
+    {
+        id: 4,
+        rname: "La Milano Pizzeria",
+        imgdata: "https://d1di2lzuh97fh2.cloudfront.net/files/1c/1ch/1chs5q.jpg?ph=6d6883b0fe",
+        address: "Pizza, Fast Food",
+        somedata: " 650 + order placed from here recently",
+        price: 30,
+        rating: "4.2",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Pizzeria",
+        imgdata: "https://www.protislank.nl/slank/wp-content/uploads/2020/09/Proteinerijke-pizza-margarita-1-500x375.png",
+        address: "Pizza, Fast Food",
+        somedata: " 650 + order placed from here recently",
+        price: 15,
+        rating: "4.2",
+        qnty:0
+    },
+    {
+        id: 6,
+        rname: "familie Pizzeria",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR76pBHeex5gMBuNYbYkraVxi_3mwyue9ucxg&usqp=CAU",
+        address: "Fast Food",
+        somedata: " 300 + order placed from here recently",
+        price: 70,
+        rating: "3.8",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "kapsalon",
+        imgdata: "http://www.koken-met-kids.be/wp-content/uploads/2011/12/pita-kapsalon.jpg",
+        address: "Fast Food",
+        somedata: "1050 + order placed from here recently",
+        price: 25,
+        rating: "4.0",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "pitta schotel",
+        imgdata: "https://www.tuanarestaurant.menu/images/menupics/schotels/pitaschotel.jpg",
+        address: "Fast Food",
+        somedata: " 1100 + order placed from here recently",
+        price: 30,
+        rating: "3.8",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "familie pitta",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu6ZQobA_ORwqCr4hkIRiCPfv_J0koJIgExQ&usqp=CAU",
+        address: "Fast Food",
+        somedata: "500 + order placed from here recently",
+        price: 100,
+        rating: "3.8",
+         qnty:0
+    },
+    {
+        id: 10,
+        rname: "water",
+        imgdata: "https://www.spa.be/img/home/product-water-group.png",
+        address: "Tea, Coffee, Shake, Beverages",
+        somedata: "500 + order placed from here recently",
+        price: 2,
+        rating: "3.2",
+         qnty:0
+    },
+    {
+        id: 11,
+        rname: "cola",
+        imgdata: "https://static.levensmiddelenkrant.nl/uploads/ac/f4/acf4987cb38e0233a019a2b98b411898.jpeg",
+        address: "Burger, Sandwich, Fast Food",
+        somedata: "2525 + order placed from here recently",
+        price: 2.5,
+        rating: "3.8",
+         qnty:0
+    },
+    {
+        id: 12,
+        rname: "fanta",
+        imgdata: "https://clipit.nl/app/uploads/2017/11/Fanta.jpg",
+        address: "Burger, Sandwich, Fast Food",
+        somedata: "2525 + order placed from here recently",
+        price: 2.5,
+        rating: "3.8",
+        qnty:0
+   },
+];
+
+export default Cardsdata;
